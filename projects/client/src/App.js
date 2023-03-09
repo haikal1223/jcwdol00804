bcd0cb08156b787379909d53cb4caa8afdff4390
alvinnaldo <naldoalvin45@gmail.com>
2023-03-09 19:55:44 +0700

    [OGWA-4] Creating client side UI UX for register

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,26 +1,15 @@
-import axios from "axios";
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { useEffect, useState } from "react";
+import SignUp from "./Pages/SignUp";
+import Page from "./Components/Page";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/greetings`
-      );
-      setMessage(data?.message || "");
-    })();
-  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {message}
-      </header>
-    </div>
+    <Page>
+      <Routes>
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Page>
   );
 }
 
