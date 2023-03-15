fdd6232bb8ce5be59d6730e6235c24cb7c72d54d
nikkovanessa <nikkovaness@gmail.com>
2023-03-15 19:59:51 +0700

    [OGWA-8] Add Forgot/Reset Password Routing

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
-// import Login from "./components/Login";
-import Home from "./Pages/Home";
-import Footer from "./Components/Footer";
-import Verify from "./Components/Verify";
-import SignUp from "./Pages/SignUp";
 import { Routes, Route } from "react-router-dom";
+// import Login from "./components/Login";
 import Page from "./Components/Page";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
+import Verify from "./Components/Verify";
+import ForgotPassword from "./Pages/ForgotPassword";
+import ResetPassword from "./Pages/ResetPassword";
 import NotFound from "./Pages/NotFound";
 
 function App() {
@@ -14,16 +16,18 @@ function App() {
     <div>
       <Page>
         <Routes>
-          {/* <Route path="/" element={<Login />} /> */}
-          <Route path="/verify-email" element={<Verify />} />
-          <Route path="/sign-up" element={<SignUp />} />
           <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          {/* <Route path="/sign-in" element={<Login />} /> */}
+          <Route path="/verify-email" element={<Verify />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </Page>
     </div>
   );
-}
+};
 
 export default App;
