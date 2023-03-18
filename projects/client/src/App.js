68aa0a57a3ba87e8a59158b35037519ff3b23080
nikkovanessa <nikkovaness@gmail.com>
2023-03-18 23:16:10 +0700

    [OGWA-7] Move Verify to Pages

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import SignUp from "./Pages/SignUp";
 import SignIn from "./Pages/SignIn";
-import Verify from "./Components/Verify";
+import Verify from "./Pages/Verify";
 import ForgotPassword from "./Pages/ForgotPassword";
 import ResetPassword from "./Pages/ResetPassword";
 import ProfileSetting from "./Pages/ProfileSetting";
