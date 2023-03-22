1207b246d7a02440b328ae0dca6bf0d0b7f46aec
alvinnaldo <naldoalvin45@gmail.com>
2023-03-22 23:41:36 +0700

    [OGWA-12] Validasi sign in dan edit profile di BE

--- projects/server/src/config/validator.js
+++ projects/server/src/config/validator.js
@@ -1,4 +1,5 @@
 const { check, validationResult } = require("express-validator");
+const { db, dbQuery } = require("./db");
 
 module.exports = {
   validateSignUp: async (req, res, next) => {
@@ -43,6 +44,70 @@ module.exports = {
           message: "Validation invalid ❌",
           error: validation.errors,
         });
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  },
+  validateSignIn: async (req, res, next) => {
+    try {
+      await check("email")
+        .isString()
+        .notEmpty()
+        .isEmail()
+        .withMessage("Invalid email format")
+        .run(req);
+      await check("password")
+        .isString()
+        .isLength({ min: 8 })
+        .withMessage("Should more than 8 characters")
+        .matches(/[a-z]/g)
+        .withMessage("Should contain at least 1 lower case letter")
+        .matches(/[A-Z]/g)
+        .withMessage("Should contain at least 1 upper case letter")
+        .matches(/[0-9]/g)
+        .withMessage("Should contain at least 1 number")
+        .run(req);
+      const validation = validationResult(req);
+      console.log(validation);
+      if (validation.isEmpty()) {
+        next();
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  },
+  validateEditProfile: async (req, res, next) => {
+    try {
+      await check("name").isString().notEmpty().run(req);
+      await check("email")
+        .isString()
+        .notEmpty()
+        .isEmail()
+        .withMessage("Invalid email format")
+        .run(req);
+      await check("birthdate")
+        .isDate()
+        .custom((value) => {
+          if (new Date(value) >= new Date()) {
+            throw new Error(
+              `Birthdate cannot beyond ${new Date().toLocaleDateString("id")}`
+            );
+          }
+          return true;
+        })
+        .optional({ nullable: true })
+        .run(req);
+      await check("gender")
+        .isIn(["male", "female"])
+        .optional({ nullable: true })
+        .run(req);
+      const validation = validationResult(req);
+      console.log(validation);
+      if (validation.isEmpty()) {
+        next();
       }
     } catch (error) {
       console.log(error);
