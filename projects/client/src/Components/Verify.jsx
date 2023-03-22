32ad3181498984edf25b54d44e26552ba4d0d87d
alvinnaldo <naldoalvin45@gmail.com>
2023-03-22 23:35:58 +0700

    [OGWA-12] Add page container to verify page

--- projects/client/src/Components/Verify.jsx
+++ projects/client/src/Components/Verify.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { API_URL } from "../helper";
 import verify from "../Assets/Verify.png";
+import Page from "./Page";
 
 function Verify() {
   const { search } = useLocation();
@@ -26,23 +27,25 @@ function Verify() {
       });
   }
   return (
-    <div className="font-sans grid justify-items-center items-center container mx-auto text-center p-10 min-h-screen">
-      <div>
-        <div className="object-fit-contain h-1/2 w-1/2 mx-auto">
-          <img src={verify} alt="verify-symbol" />
+    <Page isNavbar={false} isFooter={false}>
+      <div className="font-sans grid justify-items-center items-center container mx-auto text-center p-10 min-h-screen">
+        <div>
+          <div className="object-fit-contain h-1/2 w-1/2 mx-auto">
+            <img src={verify} alt="verify-symbol" />
+          </div>
+          <p className="text-2xl font-bold my-5">
+            Please click button below to verify your email
+          </p>
+          <p className="text-xl">{email}</p>
         </div>
-        <p className="text-2xl font-bold my-5">
-          Please click button below to verify your email
-        </p>
-        <p className="text-xl">{email}</p>
+        <button
+          className="font-semibold text-xl py-2 px-8 mt-4 bg-lime-500 text-white rounded-3xl w-full"
+          onClick={verifyEmail}
+        >
+          Verify
+        </button>
       </div>
-      <button
-        className="font-semibold text-xl py-2 px-8 mt-4 bg-lime-500 text-white rounded-3xl w-full"
-        onClick={verifyEmail}
-      >
-        Verify
-      </button>
-    </div>
+    </Page>
   );
 }
 
