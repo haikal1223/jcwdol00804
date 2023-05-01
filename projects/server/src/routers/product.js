d9c4fa13661e685cd435b747abb7ba3d23afbe8d
alvinnaldo <naldoalvin45@gmail.com>
2023-05-02 03:15:21 +0700

    [OGWA-35] - Back end config for manage product

--- projects/server/src/routers/product.js
+++ projects/server/src/routers/product.js
@@ -1,7 +1,10 @@
 const route = require("express").Router();
 const { readToken } = require("../config/token");
+const { imgUploader } = require("../config/uploader");
+const { validateAddOrEditProduct } = require("../config/validator");
 const { productController } = require("../controllers");
 
+// User
 route.get("/detail/:id", productController.getDetail);
 route.get("/product-list", productController.getProducts);
 route.get("/categories", productController.fetchCategories);
@@ -12,5 +15,53 @@ route.patch(
   productController.adjustStockAfterOrder
 );
 route.get("/get-branch-list", productController.getBranchList);
+
+// Admin
+route.get("/admin/product-list", readToken, productController.getProductsAdmin);
+route.get(
+  "/admin/product-detail/:id",
+  readToken,
+  productController.getProductDetailAdmin
+);
+route.post(
+  "/admin/add-product",
+  readToken,
+  validateAddOrEditProduct,
+  productController.addProductAdmin
+);
+route.patch(
+  "/admin/add-product-img",
+  readToken,
+  imgUploader("/imgProduct", "IMGPRODUCT").array("images", 1),
+  productController.addProductImg
+);
+route.patch(
+  "/admin/edit-product/:id",
+  readToken,
+  validateAddOrEditProduct,
+  productController.editProduct
+);
+route.patch(
+  "/admin/edit-product-img/:id",
+  readToken,
+  imgUploader("/imgProduct", "IMGPRODUCT").array("images", 1),
+  productController.editProductImg
+);
+route.patch(
+  "/admin/delete-product/:id",
+  readToken,
+  productController.deleteProduct
+);
+route.patch(
+  "/admin/set-featured-product/:id",
+  readToken,
+  productController.setFeaturedProduct
+);
+route.patch("/admin/add-stock/:id", readToken, productController.addStock);
+route.patch(
+  "/admin/adjust-stock/:id",
+  readToken,
+  productController.adjustStock
+);
 
 module.exports = route;
